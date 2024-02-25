0ef9e8c4640b0bac7102c395d2375776b7a8c87f
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-02-25 18:31:15 +0000

    Setting and retrieving environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!$gik0+bow!3k(t9(zd8+wc%77sl!ff#%o$hw3d52$7lyquiww'
+# SECRET_KEY = 'django-insecure-!$gik0+bow!3k(t9(zd8+wc%77sl!ff#%o$hw3d52$7lyquiww'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
